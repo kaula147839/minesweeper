91abec4af2e08e3f2933807f7eecc0947fbaeccf
kaula147839 <kay147839@gmail.com>
2024-05-26 16:47:59 +0800

    創視窗

--- mineTEST.py
+++ mineTEST.py
@@ -4,14 +4,14 @@ running = True
 Bomb = [[0]*10 for i in range(10)]#地雷本盤
 show_Bomb = [[0]*10 for i in range(10)]#顯示地雷盤
 count = 0
-while(count <= 15):
+while(count <= 10):
     x = int (random.randrange(0,10))
     y = int (random.randrange(0,10))
     if Bomb[x][y] == 0:
         Bomb[x][y] = 1
         count += 1
 
-def detect_Bomb(x,y):#創韓式烤布雷
+def detect_Bomb(x,y,check):#創韓式烤布雷
     count = 0
     if Bomb[x][y] == 1:
         print("GG")
@@ -24,17 +24,28 @@ def detect_Bomb(x,y):#創韓式烤布雷
                 if x1 >= 0 and x1 <= 9 and y1 >= 0 and y1 <= 9:
                     if Bomb[x1][y1] == 1:
                         count += 1
+        check = bool(1)
         if count >= 1:
-            show_Bomb[x][y] = count  #顯示是否有地雷
+            show_Bomb[x][y] = count  #顯示地雷數
         else:
             for a in range(-1,2):
                 for b in range(-1,2):
-                    detect_Bomb(x+a,y+b)
+                    x_new = x + a
+                    y_new = y + b 
+                    if x_new > 9 :
+                        x_new = 9 
+                    if x_new < 0 :
+                        x_new = 0 
+                    if y_new > 9 :
+                        y_new = 9 
+                    if y_new < 0 :
+                        y_new = 0 
+                    detect_Bomb(x_new,y_new,0)
         return True
 while(running == True):
     print(np.array(Bomb),"\n")
     print(np.array(show_Bomb))
     x = int (input())
     y = int (input())
-    running = detect_Bomb(x,y)
+    running = detect_Bomb(x,y,0)
 
--- minesweeper.py
+++ minesweeper.py
@@ -32,52 +32,67 @@ def draw_text(surf, text, size, x, y):
 Bomb = [[0]*10 for i in range(10)]#地雷本盤
 show_Bomb = [[0]*10 for i in range(10)]#顯示地雷盤
 count = 0
-while(count <= 15):
-    x = random.randrange(0,10)
-    y = random.randrange(0,10)
+#烤布雷
+while(count <= 10):
+    x = int (random.randrange(0,10))
+    y = int (random.randrange(0,10))
     if Bomb[x][y] == 0:
         Bomb[x][y] = 1
         count += 1
-count = 0
 def detect_Bomb(x,y):#創韓式烤布雷
+    count = 0
     if Bomb[x][y] == 1:
-        running = False
+        return False
     else:
         for i in range(-1,2):
             for j in range(-1,2):
-                if Bomb[x+i][y+j] == 1:
-                    count += 1
+                x1 = x+i
+                y1 = y+j
+                if x1 >= 0 and x1 <= 9 and y1 >= 0 and y1 <= 9:
+                    if Bomb[x1][y1] == 1:
+                        count += 1
+        #check = bool(1)
         if count >= 1:
-            show_Bomb[x][y] = count#顯示是否有地雷
+            show_Bomb[x][y] = count  #顯示地雷數
+            count = int(count)
+            draw_text(screen,count,30,50*x+10,50*y+10)
         else:
             for a in range(-1,2):
                 for b in range(-1,2):
-                    detect_Bomb(x+a,y+b)
-
-
-        
-
-
+                    detect_Bomb(x+a,y+b,0)
+        return True
+def draw_Bomb(surf, x, y):#畫烤布雷的底盤
+    BAR_LENGTH = 50
+    BAR_HEIGHT = 50
+    outline_rect = pygame.Rect(x, y, BAR_LENGTH, BAR_HEIGHT)#定義外框
+    pygame.draw.rect(surf, BLACK, outline_rect, 2)#畫外框
+def get_Bomb(x,y,mouse_press):#偵測勞贖吃烤布雷
+    if y >= 100:
+        x = int(mouse_x / 50)
+        y = int(mouse_y / 50)
+    if mouse_press == (1,0,0):
+        detect_Bomb(x,y)
 #迴圈
 running = True
 
 while running:
     clock.tick(FPS)
-    mouse_x,mouse_y= pygame.mouse.get_pos()
-    mouse_press = pygame.mouse.get_pressed()
-
+    mouse_x,mouse_y = pygame.mouse.get_pos()
+    mouse_press = pygame.mouse.get_pressed(num_buttons=3)
     #取得輸入
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
-
-    #更新遊戲
     #畫面顯示
     screen.fill(WHITE)
+    for size_x in range(10):
+        for  size_y in range(10):
+            draw_Bomb(screen,50 * size_x,100 +  50 * size_y)
+    get_Bomb(mouse_x,mouse_y,mouse_press)
     draw_text(screen, str(mouse_x),18,WIDTH/2,10)
-    draw_text(screen, str(mouse_y),18,WIDTH/2-10,10)
-    draw_text(screen, str(mouse_press),18,WIDTH/2,28)
+    draw_text(screen, str(mouse_y),18,WIDTH/2+25,10)
+    #draw_text(screen, str(mouse_press),18,WIDTH/2,28)
 
     pygame.display.update()
-pygame.quit() 
+draw_text(screen,str("GAME OVER"),40,WIDTH/2,HEIGHT/2)
+#pygame.quit() 
